3b6d6e2576c1f2e43ab8894faccdcce012e728e9
dyenaan <91904892+dyenaan@users.noreply.github.com>
2023-07-22 11:55:19 -0600

    Delete, Update & Save features
    Features to delete, update and save notes through the frontend were added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,18 @@ import { Note as NoteModel} from './models/note';
 import Note from './components/Note';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import styles from "./styles/NotesPage.module.css"
+import styleUtils from "./styles/utils.module.css"
 import * as NotesApi from "./network/notes_api";
-import AddNoteDialog from './components/AddNoteDialog';
-
+import AddNoteDialog from './components/AddEditNoteDialog';
+import {FaPlus} from "react-icons/fa";
+import AddEditNoteDialog from './components/AddEditNoteDialog';
 
 function App() {
 
   const [notes, setNotes] = useState<NoteModel[]>([]);
 
   const [showAddNoteDialog, setShowAddNoteDialog] = useState(true);
+  const [noteToEdit, setNoteToEdit] = useState<NoteModel|null>(null);
 
   useEffect(() => {
     async function loadNotes(){
@@ -26,22 +29,56 @@ function App() {
     loadNotes();
   }, []);
 
+  async function deleteNote(note: NoteModel){
+    try{
+      await NotesApi.deleteNote(note._id);
+      setNotes(notes.filter(existingNote => existingNote._id !== note._id))
+    }catch(error){
+      console.error(error);
+      alert(error);
+    }
+  }
+
   return (
     <Container>
-      <Button onClick={() => setShowAddNoteDialog(true)}>
+      <Button 
+      className={`mb-4 ${styleUtils.blockCenter} ${styleUtils.flexCenter}`}
+      onClick={() => setShowAddNoteDialog(true)}>
+        <FaPlus/>
         Add new note
       </Button>
       <Row xs = {1} md = {2} xl = {3} className = "g-4">
         {notes.map(note => (
           <Col key = {note._id}>
-            <Note note ={note} className = {styles.note}/>
+            <Note 
+            note ={note} 
+            className = {styles.note}
+            onNoteClicked={setNoteToEdit}
+            onDeleteNoteClicked={deleteNote}
+            />
           </Col>
         ))}
       </Row>
       { showAddNoteDialog &&
-        <AddNoteDialog 
+        <AddNoteDialog
         onDismiss={ () => setShowAddNoteDialog(false)}
+        onNoteSaved={(newNote) => {
+          setNotes([...notes, newNote])
+          setShowAddNoteDialog(false);
+        }}
+
+          
         />
+      }
+      {noteToEdit &&
+      <AddEditNoteDialog
+      noteToEdit={noteToEdit}
+      onDismiss={()=> setNoteToEdit(null)}
+      onNoteSaved={(updatedNote)=>{
+        setNotes(notes.map(existingNote => existingNote._id === updatedNote._id ? updatedNote : existingNote));
+        setNoteToEdit(null);
+      }}
+      />
       }
     </Container>
   );
